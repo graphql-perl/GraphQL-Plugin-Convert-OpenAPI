37c19aa1db4c414749094f3212906e006be1615e
Ed J <mohawk2@users.noreply.github.com>
2019-02-08 23:21:47 +0000

    v0.19

--- lib/GraphQL/Plugin/Convert/OpenAPI.pm
+++ lib/GraphQL/Plugin/Convert/OpenAPI.pm
@@ -7,7 +7,7 @@ use GraphQL::Debug qw(_debug);
 use JSON::Validator::OpenAPI::Mojolicious;
 use OpenAPI::Client;
 
-our $VERSION = "0.18";
+our $VERSION = "0.19";
 use constant DEBUG => $ENV{GRAPHQL_DEBUG};
 
 my %TYPEMAP = (
